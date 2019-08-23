53f78c4c7a0815704e8d89681a997f4eb3a9f7a7
anchittandon <mt1170772@iitd.ac.in>
2019-08-23 14:55:19 +0530

    Remove hist eq. and add memory of axis

--- Medial_Axis_Finder/visA1.py
+++ Medial_Axis_Finder/visA1.py
@@ -1,28 +1,33 @@
 import numpy as np
 import cv2
+from sklearn.cluster import KMeans
 
 video = cv2.VideoCapture('test_vids\\1.mp4')
 frame_width = int(video.get(3))
 frame_height = int(video.get(4))
+kmeans = KMeans(n_clusters=2)
 
 fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
 kernel = np.ones((5,5),np.uint8)
-out = cv2.VideoWriter("test_vids\\output1Contrast.avi",cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
-clahe = cv2.createCLAHE(clipLimit=10, tileGridSize=(8,8))
+out = cv2.VideoWriter("test_vids\\removedhisteq1.avi",cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+# clahe = cv2.createCLAHE(clipLimit=10, tileGridSize=(8,8))
 
+a1=0
+a2=0
+prev = [0,0,0]
 while(1):
     ret, orig_frame = video.read()
     # cv2.imshow("frame",orig_frame)
     if not ret:
         break
 
-    lab = cv2.cvtColor(orig_frame, cv2.COLOR_BGR2LAB)
-    lab_planes = cv2.split(lab)
-    lab_planes[0] = clahe.apply(lab_planes[0])
-    lab = cv2.merge(lab_planes)
-    bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
+    # lab = cv2.cvtColor(orig_frame, cv2.COLOR_BGR2LAB)
+    # lab_planes = cv2.split(lab)
+    # lab_planes[0] = clahe.apply(lab_planes[0])
+    # lab = cv2.merge(lab_planes)
+    # bgr = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
     
-    frame = fgbg.apply(bgr)
+    frame = fgbg.apply(orig_frame)
     
     frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
     
@@ -32,8 +37,6 @@ while(1):
     # cv2.imshow("frame",edges)
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, maxLineGap=100)
     
-    a1=0
-    a2=0
     b1=-10000000
     b2=100000000
     count = 0
@@ -55,6 +58,7 @@ while(1):
         cleaned_lines = [x for x in slope_list if abs(x[0]-meu)<sigma]
     
     if len(cleaned_lines) > 0:
+
         for line in cleaned_lines:
             x1, y1, x2, y2 = line[1]
             m = line[0]
@@ -63,20 +67,29 @@ while(1):
                 x_list1.append((x2,m))
                 b1 = max(b1,y1)
                 # b2 = min(b2,y2)
-                # cv2.line(orig_frame, (int(x1),y1), (int(x2),0), (255,0,0), 2)
+                cv2.line(orig_frame, (int(x1),y1), (int(x2),0), (255,0,0), 2)
             else:
                 x1 = -(y1/m) + x1
                 x_list1.append((x1,m))
                 b1 = max(b1,y2)
                 # b2 = min(b2,y1)
-                # cv2.line(orig_frame, (int(x1),0), (int(x2),y2), (255,0,0), 2)
+                cv2.line(orig_frame, (int(x1),0), (int(x2),y2), (255,0,0), 2)
             # cv2.line(bgr, (int(x1),y1), (int(x2),y2), (255,0,0), 2)
-        x_list1.sort()
-        a2= int(x_list1[len(x_list1)//2][0]+0.5)
+        # x_list1.sort()
+        x_list = [[x[0],] for x in x_list1]
+        if len(x_list)>1:
+            clusters = kmeans.fit(x_list)
+            centers = kmeans.cluster_centers_
+            print(centers)
+            a2= int((centers[0][0]+centers[1][0])/2)
+            # a2= int(x_list1[len(x_list1)//2][0]+0.5)
         m = x_list1[len(x_list1)//2][1]
         a1 = int((b1/m)+a2+0.5)
         cv2.line(orig_frame, (a1,b1), (a2,0), (0,0,255), 2)
-    
+        prev = [a1,a2,b1]
+    else:
+        a1,a2,b1 = prev
+        cv2.line(orig_frame, (a1,b1), (a2,0), (0,0,255), 2)
     cv2.imshow("frame", orig_frame)
     # cv2.imshow("frame2",frame)
     out.write(orig_frame)
@@ -88,4 +101,3 @@ while(1):
 video.release()
 out.release()
 cv2.destroyAllWindows()
-    
